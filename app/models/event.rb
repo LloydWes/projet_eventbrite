7369ac840cfb69b56e5546f47f3367ca3a24ca70
Lloyd <lloydwestbury@gmail.com>
2019-05-06 22:37:02 +0200

    d

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :user#, class_name: 'User'# :foreign_key => 'admin_id'
+  belongs_to :admin, class_name: 'User'# :foreign_key => 'admin_id'
   has_many :attendances
   # belongs_to :user#, :foreign_key => 'admin_id'
 end
