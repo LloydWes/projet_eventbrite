f8521e3f0d4d6392e7ceb40d45b3139ed2e25202
LloydWes <48556811+LloydWes@users.noreply.github.com>
2019-05-07 11:15:36 +0200

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,6 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  # :user_name => 'Lloyd_thp', #ENV['SENDGRID_LOGIN'],
-  # :password => 'Faurecia-22-66', #ENV['SENDGRID_PWD'],
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
   :domain => 'monsite.fr',
